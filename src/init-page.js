04b4676b8987adf4852e85e8a117c15947b5c843
Adam Gordon <adambaumgordon@gmail.com>
2021-09-01 12:16:48 -0600

    initialize webpack

---
+++ src/init-page.js
@@ -0,0 +1,7 @@
+function initPage () {
+    const element = document.createElement("div");
+    element.textContent = "Hello world!";
+    document.body.appendChild(element);
+}
+
+export default initPage;
